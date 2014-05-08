3c680ce49e2b6fec3659ca385ff17e4f5344f39e
Gotzillaz Chinthanet <got.bodin@gmail.com>
2014-05-09 03:25:25 +0700

    Add Gamemap Attributes (gridsize,girdpos) and Gameball gridpos

--- main.py
+++ main.py
@@ -19,6 +19,19 @@ class LokiColorG(Widget):
     pass
 
 class GameMap(GridLayout):
+    gridsize = 4
+    gridpos = [[]]
+
+    def updateGridPos(self):
+        for x in self.children:
+            print x.id
+            pr = int(x.id[1])
+            pc = int(x.id[2])
+            self.gridpos[pr][pc] = (x.center_x,x.center_y)
+
+    def createGridPos(self, num):
+        self.gridpos = [[(0,0) for x in xrange(num)] for y in xrange(num)]
+
     def __init__(self,**kwargs):
         super(GameMap, self).__init__(**kwargs)
         for x in xrange(4):
@@ -27,8 +40,13 @@ class GameMap(GridLayout):
         for x in self.children:
             print x.pos,x.parent.pos,x.text,x.center_x,x.center_y
         print self.pos,self.center_x,self.center_y
+        self.createGridPos(self.gridsize)
+        self.updateGridPos()
+        print self.gridpos
     
 class GameBall(Widget):
+    ballgrid = [1,1]
+
     def changePos(self,x,y):
         self.x = x
         self.y = y
@@ -45,8 +63,6 @@ class GameTab(Widget):
     gameball = ObjectProperty(None)
     gamemap = ObjectProperty(None)
 
-    #map = []
-
     def pt(self,x):
         #self.gameball.size = [30,30]
         #self.gameball.
@@ -54,11 +70,17 @@ class GameTab(Widget):
         print self.children
         for x in self.children[1].children:
             print x.center
+            #print x.id
+        self.gamemap.updateGridPos()
+        print self.gamemap.gridpos
         print self.children[0].center, "zzzz"
         print self.children[0].size,self.children[0]
         print self.center,self
         radius = self.gameball.size[0]/2.0
-        self.gameball.changePos(self.children[1].children[randint(0,15)].center_x-radius,self.children[1].children[randint(0,15)].center_y-radius)
+        bpos = self.gameball.ballgrid
+        print bpos
+        #self.gameball.changePos(self.children[1].children[bpos[0]].center_x-radius,self.children[1].children[bpos[1]].center_y-radius)
+        self.gameball.changePos(self.gamemap.gridpos[bpos[0]][bpos[1]][0]-radius,self.gamemap.gridpos[bpos[0]][bpos[1]][1]-radius)
         self.gameball.changeColor()
         #self.gameball.center = self.children[1].children[randint(0,15)].center
         #self.children[0].center = self.children[1].children[randint(0,15)].center
