4f09802c034991103c12d867c434b7d7f98d9bd6
Gotzillaz Chinthanet <got.bodin@gmail.com>
2014-05-14 03:44:58 +0700

    change some obj create method + add readStage Method

--- main.py
+++ main.py
@@ -41,18 +41,21 @@ class GamePlate(Widget):
         self.canvas.add(GameColor().getColor(mycolor))
         self.canvas.add(Rectangle(size=self.size,pos=self.pos))
 
-    def __init__(self,**kwargs):
+    def __init__(self,color,**kwargs):
         super(GamePlate, self).__init__(**kwargs)
-        self.changeColor(color)
+        self.color = color
+        self.changeColor(self.color)
 
 class GameMap(GridLayout):
     gridsize = 5
     gridpos = [[]]
-    color_table_now = [[]]
-    map_table_now = [[]]
+    color_table = [[]]
+    map_table = [[]]
     
     def readMap(self,map_table,color_table,size):
         self.gridsize = size
+        self.map_table = [[x for x in y] for y in map_table]
+        self.color_table = [[x for x in y] for y in color_table]
 
     def updateGridPos(self):
         for x in self.children:
@@ -64,11 +67,12 @@ class GameMap(GridLayout):
     def createGridPos(self, num):
         self.gridpos = [[(0,0) for x in xrange(num)] for y in xrange(num)]
 
-    def __init__(self,**kwargs):
+    def __init__(self,map_t,color_t,si,**kwargs):
         super(GameMap, self).__init__(**kwargs)
-        for x in xrange(5):
-            for y in xrange(5):
-                self.add_widget(GamePlate(id='m'+str(x)+str(y),size=[100,100],text=str(x)+' '+str(y),color='Green'))
+        self.readMap(map_t,color_t,si)
+        for x in xrange(self.gridsize):
+            for y in xrange(self.gridsize):
+                self.add_widget(GamePlate(id='m'+str(x)+str(y),size=[100,100],text=str(x)+' '+str(y),color=self.color_table[x][y]))
                 #self.add_widget(Button(id='m'+str(x)+str(y),size=[100,100],text=str(x)+' '+str(y)))
         for x in self.children:
             print x.pos,x.parent.pos,x.text,x.center_x,x.center_y
@@ -78,8 +82,10 @@ class GameMap(GridLayout):
         print self.gridpos
     
 class GameBall(Widget):
-    ballgrid = [1,3]
-    oldgrid = [1,3]
+    ballgrid = [0,0]
+    oldgrid = [0,0]
+    endgrid = [1,1]
+    ball_color = 'White'
     #self.gamemap.gridpos[bpos[0]][bpos[1]][0]-radius
     def smoothBall(self,tt):
         if self.ballgrid != self.oldgrid:
@@ -99,12 +105,19 @@ class GameBall(Widget):
                 self.oldgrid[1] = self.ballgrid[1]
                 print "FIN"
         else:
+            #print self.parent,"AAAAAAAAAAAA"
             self.pos[0] = self.parent.gamemap.gridpos[self.ballgrid[0]][self.ballgrid[1]][0]-self.size[0]/2
             self.pos[1] = self.parent.gamemap.gridpos[self.ballgrid[0]][self.ballgrid[1]][1]-self.size[1]/2
 
     def changeGrid(self,x,y):
         self.ballgrid[0] = x
         self.ballgrid[1] = y
+
+    def setFirstStat(self,start,end):
+        self.ballgrid[0] = self.oldgrid[0] = start[0]
+        self.ballgrid[1] = self.oldgrid[1] = start[1]
+        self.endgrid[0] = end[0]
+        self.endgrid[1] = end[1]
 
     def changePos(self,x,y):
         self.x = x
@@ -113,7 +126,7 @@ class GameBall(Widget):
     def changeColor(self):
         self.canvas.clear()
         self.canvas.add(Color(randint(0,100)/100.0,randint(0,100)/100.0,randint(0,100)/100.0))    
-        self.canvas.add(GameColor().getColor('White'))
+        self.canvas.add(GameColor().getColor('Green'))
         self.canvas.add(Ellipse(size=self.size,pos=self.pos))
 
     def __init__(self,**kwargs):
@@ -121,6 +134,7 @@ class GameBall(Widget):
         Clock.schedule_interval(self.smoothBall,1/60.0)
 
 class GameTab(Widget):
+    num_stage = 0
     gameball = ObjectProperty(None)
     gamemap = ObjectProperty(None)
     stepmethod = ''
@@ -129,14 +143,22 @@ class GameTab(Widget):
     def toggle(self):
         self.goenable = not self.goenable
 
-    def readMap(self,stage):
-        # Open map file
+    def readStage(self):
+        # Open stage file
         num_color = 3
         map_size = 5
         color_list = ['Red','Green','Blue']
-        map_table[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+        color_table = [['White','White','White','White','White'],['White','White','White','White','White'],['White','White','White','White','White'],['White','White','White','White','White'],['White','White','White','White','White']]
+        map_table = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
         start_pos = [1,1]
         end_pos = [4,4]
+        print self.center ,"KUY"
+        self.gamemap = GameMap(map_t=map_table ,color_t=color_table,si=map_size,size=[400,400],center=self.center,rows=map_size,cols=map_size,spacing=5,size_hint=[None,None])
+        self.add_widget(self.gamemap)
+        self.gameball = GameBall(size=[50,50])
+        self.add_widget(self.gameball)
+        #self.gamemap.readMap(map_table, color_table, map_size)
+        self.gameball.setFirstStat(start_pos, end_pos)
 
     def changeStep(self, way):
         self.stepmethod = way
@@ -164,6 +186,9 @@ class GameTab(Widget):
         #self.gameball.size = [30,30]
         #self.gameball.
         print "xx" 
+        print "GameTab Center",self.center
+        print "GameTab size",self.size
+        self.gamemap.center = self.center
         print self.children
         for x in self.children[1].children:
             print x.center
@@ -172,9 +197,9 @@ class GameTab(Widget):
         tmpc = ['Red','Blue','Green','White']
         tmps = [True,False]
         for x in self.gamemap.children:
-            x.changeColor('Pink')
-            x.star = tmps[randint(0,1)]
-            x.hasStar()
+            x.changeColor(x.color)
+            #x.star = tmps[randint(0,1)]
+            #x.hasStar()
         print self.gamemap.gridpos
         print self.children[0].center, "zzzz"
         print self.children[0].size,self.children[0]
@@ -195,6 +220,8 @@ class GameTab(Widget):
 
     def __init__(self,**kwargs):
         super(GameTab,self).__init__(**kwargs)
+        self.pos = [0,0]
+        self.readStage()
         Clock.schedule_interval(self.pt,1)
 
 class GameScreen(Screen):
